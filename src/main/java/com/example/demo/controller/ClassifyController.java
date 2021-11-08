56ed84b939b6cca538ab98fb6ef01de6b5d4aeee
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2021-11-08 22:14:10 +0800

    Add files via upload

--- src/main/java/com/example/demo/controller/ClassifyController.java
+++ src/main/java/com/example/demo/controller/ClassifyController.java
@@ -6,8 +6,10 @@ import java.util.Map;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -45,6 +47,27 @@ public class ClassifyController {
 		}
 		classifyRepository.save(classify);
 		return classify;
+	}
+	
+	@PutMapping(value = {"/{id}", "/update/{id}"})
+	@Transactional
+	public Boolean update(@PathVariable("id") Integer id, @RequestBody Map<String, String> map) {
+		Classify classify = get(id);
+		classify.setName(map.get("name"));
+		if(map.get("tx") == null) {
+			classify.setTx(false);
+		} else {
+			classify.setTx(true);
+		}
+		classifyRepository.saveAndFlush(classify);
+		return true;
+	}
+	
+	@DeleteMapping(value = {"/{id}", "/delete/{id}"})
+	@Transactional
+	public Boolean delete(@PathVariable("id") Integer id) {
+		classifyRepository.deleteById(id);
+		return true;
 	}
 	
 	
