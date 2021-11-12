852034d17c3fa3e22524947f262d354e8d1c45f9
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2021-11-12 22:20:20 +0800

    Add files via upload

--- src/main/java/com/example/demo/sql/sample.sql
+++ src/main/java/com/example/demo/sql/sample.sql
@@ -1,1 +1,3 @@
+-- jdbc:mysql://localhost:3306/portfolio?serverTimezone=UTC&characterEncoding=utf-8&useUnicode=true
+-- jdbc:mysql://localhost:3306/portfolio?serverTimezone=GMT%2B8&characterEncoding=utf-8&useUnicode=true
 CREATE DATABASE portfolio CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
