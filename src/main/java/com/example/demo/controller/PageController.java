3a7bd837735c8f5edae0b726875f96f0681c879e
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2021-11-12 18:48:13 +0800

    Add files via upload

---
+++ src/main/java/com/example/demo/controller/PageController.java
@@ -0,0 +1,30 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/page")
+public class PageController {
+	
+	@RequestMapping("/home")
+	public String getHomePage() {
+		return "home";
+	}
+	
+	@RequestMapping("/classify")
+	public String getClassifyPage() {
+		return "classify";
+	}
+	
+	@RequestMapping("/investor")
+    public String getInvestorPage() {
+        return "investor";
+    }
+    
+    @RequestMapping("/tstock")
+    public String getTStockPage() {
+        return "tstock";
+    }
+	
+}
